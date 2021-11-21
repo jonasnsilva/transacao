4b3b2d49e511beb2e9729be65abfb8c4cd7ec19a
Jonas Nascimento da Silva <contato@jonas.dev.br>
2021-11-21 10:46:43 -0300

    Alteração do nome do job e inserção da instância do objeto notification ao disparar uma queue.

--- app/Jobs/SendNotificationTransactionJob.php
+++ app/Jobs/SendNotificationTransactionJob.php
@@ -2,20 +2,24 @@
 
 namespace App\Jobs;
 
-class SendNotificationTransaction extends Job
+use App\Models\Notification;
+
+class SendNotificationTransactionJob extends Job
 {
 
-    public $tries = 10;
+    public $tries = 15;
     public $backoff = [120, 240];
+
+    private Notification $notification;
 
     /**
      * Create a new job instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(Notification $notification)
     {
-        //
+        $this->$notification = $notification;
     }
 
     /**
